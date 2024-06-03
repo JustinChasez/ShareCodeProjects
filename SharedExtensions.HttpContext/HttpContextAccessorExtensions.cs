cb76f88056e72f7ba98ffe80c1176b606e0f6586
Justin Nguyen <justinchasez04121990@gmail.com>
2024-06-03 15:14:20 +0700

    HttpContextAccessorExtensions update to direct method calls to its HttpContext

--- SharedExtensions.HttpContext/HttpContextAccessorExtensions.cs
+++ SharedExtensions.HttpContext/HttpContextAccessorExtensions.cs
@@ -21,15 +21,7 @@ internal static class HttpContextAccessorExtensions
     /// 		Thrown if the HttpContext is not available
     ///  </exception>
     public static void StoreValue<TItem>(this IHttpContextAccessor httpContextAccessor, TItem value)
-    {
-        if (httpContextAccessor.HttpContext?.Items == null)
-            throw new InvalidOperationException($"No HttpContext available");
-
-        if (!httpContextAccessor.HttpContext.Items.TryAdd(typeof(TItem), value)) // cannot add due to the key existence
-        {
-            httpContextAccessor.HttpContext.Items[typeof(TItem)] = value; // try to override the value
-        }
-    }
+        => httpContextAccessor.HttpContext.StoreValue(value);
 
     /// <summary>
     ///		Retrieves the stored object from the current HttpContext, using the key of typeof(<typeparamref name="TItem"/>)
@@ -44,19 +36,8 @@ internal static class HttpContextAccessorExtensions
     /// <exception cref="InvalidOperationException">
     ///		Thrown if the HttpContext is not available
     /// </exception>
-    public static TItem RetrieveValue<TItem>(this IHttpContextAccessor httpContextAccessor)
-    {
-        if (httpContextAccessor.HttpContext?.Items == null)
-        {
-            return default;
-        }
-
-        if (httpContextAccessor.HttpContext.Items.TryGetValue(typeof(TItem), out var itemObject) &&
-            itemObject is TItem result)
-            return result;
-
-        return default;
-    }
+    public static TItem RetrieveValue<TItem>(this IHttpContextAccessor httpContextAccessor) 
+        => httpContextAccessor.HttpContext.RetrieveValue<TItem>();
 
 
     ///  <summary>
@@ -71,16 +52,8 @@ internal static class HttpContextAccessorExtensions
     ///  <exception cref="InvalidOperationException">
     /// 		Thrown if the HttpContext is not available
     ///  </exception>
-    public static void StoreValue<TItem>(this IHttpContextAccessor httpContextAccessor, string storeKey, TItem value)
-    {
-        if (httpContextAccessor.HttpContext?.Items == null)
-            throw new InvalidOperationException($"No HttpContext available");
-
-        if (!httpContextAccessor.HttpContext.Items.TryAdd(storeKey, value)) // cannot add due to the key existence
-        {
-            httpContextAccessor.HttpContext.Items[storeKey] = value; // try to override the value
-        }
-    }
+    public static void StoreValue<TItem>(this IHttpContextAccessor httpContextAccessor, string storeKey, TItem value) 
+        => httpContextAccessor.HttpContext.StoreValue(storeKey, value);
 
     ///  <summary>
     /// 		Retrieves the stored object from the current HttpContext using the given <see cref="storeKey"/>
@@ -97,52 +70,5 @@ internal static class HttpContextAccessorExtensions
     /// 		Thrown if the HttpContext is not available
     ///  </exception>
     public static TItem RetrieveValue<TItem>(this IHttpContextAccessor httpContextAccessor, string storeKey)
-    {
-        if (httpContextAccessor.HttpContext?.Items == null)
-        {
-            return default;
-        }
-
-        if (httpContextAccessor.HttpContext.Items.TryGetValue(storeKey, out var itemObject) &&
-            itemObject is TItem result)
-            return result;
-
-        return default;
-    }
-
-    ///  <summary>
-    /// 		Tries retrieving the stored object from the current HttpContext using the given <see cref="storeKey"/>
-    ///  </summary>
-    ///  <typeparam name="TItem">
-    /// 		The type of stored object expected to retrieve
-    ///  </typeparam>
-    ///  <param name="httpContextAccessor"></param>
-    ///  <param name="storeKey">The key for retrieving the object</param>
-    ///  <returns>
-    /// 		<c>true</c> if <typeparamref name="TItem"/> object is found; otherwise, <c>false</c>
-    ///  </returns>
-    ///  <exception cref="InvalidOperationException">
-    /// 		Thrown if the HttpContext is not available
-    ///  </exception>
-    public static bool TryRetrieveValue<TItem>(this IHttpContextAccessor httpContextAccessor,
-                                               string                    storeKey,
-                                               out TItem                 result)
-    {
-        result = default;
-
-        if (httpContextAccessor.HttpContext?.Items == null)
-        {
-            return false;
-        }
-
-        if (httpContextAccessor.HttpContext.Items.TryGetValue(storeKey, out var itemObject) &&
-            itemObject is TItem tItem)
-        {
-            result = tItem;
-
-            return true;
-        }
-
-        return false;
-    }
+        => httpContextAccessor.HttpContext.RetrieveValue<TItem>(storeKey);
 }
